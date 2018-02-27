8e3b0d349ead1078015c9a19cd1ff1fc3d2991ca
Jesus Mota <j3sus1506@gmail.com>
2018-02-26 23:12:41 -0400

    added support for dinamyc references!

--- index.js
+++ index.js
@@ -2,29 +2,7 @@ const faker = require("faker");
 
 class Seeder {
   async seed(model, count) {
-    if (!model.collection) return;
-    let triesCount = 0;
-
-    console.log("Seeding:", model.collection.collectionName);
-    let doc;
-    for (var i = 0; i < count; i++) {
-      try {
-        doc = await this.makeFakeDoc(model);
-        await new model(doc).save();
-      } catch (e) {
-        i--;
-        triesCount++;
-        if (triesCount == 1000) {
-          console.log(
-            "ERROR THIS MODEL HAVE SOME VALIDATIONS THAT CANNOT LET CREATE A FAKE DOC!"
-          );
-          console.log("Models Generated:", i);
-          console.log("Error Getted", e);
-          break;
-        }
-      }
-    }
-    console.log("Finished to Seed:", model.collection.collectionName);
+    await this.seedWithDefaults(model, count, {});
   }
 
   async seedWithDefaults(model, count, defaults) {
@@ -41,9 +19,6 @@ class Seeder {
         i--;
         triesCount++;
         if (triesCount == 1000) {
-          console.log(
-            "ERROR THIS MODEL HAVE SOME VALIDATIONS THAT CANNOT LET CREATE A FAKE DOC!"
-          );
           console.log("Models Generated:", i);
           console.log("Error Getted", e);
           break;
@@ -69,14 +44,15 @@ class Seeder {
       ) {
         doc[paths[i]] = defaults[paths[i]];
       } else {
-        var value = await this.fakePathParamByModel(model, paths[i]);
+        if (doc[paths[i]]) continue;
+        var value = await this.fakePathParamByModel(model, paths[i], doc);
         if (value) doc[paths[i]] = typeof value == "function" ? value() : value;
       }
     }
     return doc;
   }
 
-  async fakePathParam(path) {
+  async fakePathParam(path, paths, doc) {
     if (!path) return null;
 
     path.options = path.options || {};
@@ -85,9 +61,11 @@ class Seeder {
       let how = Number(prop.replace("optional:", "").replace("%", ""));
       if (Math.floor(Math.random() * 100) > how) return null;
     }
+
     if (path.options.enum) return this.randomValueFrom(path.options.enum);
     if (path.instance == "Array") return this.makeFakeArray(path);
     if (path.schema) return this.makeFakeDoc(path);
+    if (path.options.refPath) return await this.makeMultiRef(path, paths, doc);
     if (path.options.ref) return await this.makeFakeRef(path.options.ref);
 
     if (prop) {
@@ -98,11 +76,30 @@ class Seeder {
     return null;
   }
 
-  async fakePathParamByModel(model, pathName) {
+  async makeMultiRef(path, paths, seededDoc) {
+    if (!this.models)
+      throw new Error(
+        "please set the models with 'mseed.setModels(models)' to make ref fill function work"
+      );
+    if (!paths[path.options.refPath].options.enum)
+      throw new Error(
+        "the kind field for dynamic reference must have an enum with posibles Models"
+      );
+
+    const ref = this.randomValueFrom(paths[path.options.refPath].options.enum);
+    seededDoc[path.options.refPath] = ref;
+    let count = await this.models[ref].count();
+    let random = Math.floor(Math.random() * count);
+    let doc = await this.models[ref].findOne().skip(random);
+    if (!doc) doc = await this.createAndSaveFakeDoc(this.models[ref]);
+    return doc ? doc.id : null;
+  }
+
+  async fakePathParamByModel(model, pathName, doc) {
     var path;
     if (model.schema) path = model.schema.paths[pathName];
     else path = model.paths[pathName];
-    return this.fakePathParam(path);
+    return this.fakePathParam(path, model.schema.paths, doc);
   }
 
   fakerProp(obj, prop) {
